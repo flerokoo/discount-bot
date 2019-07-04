0fa27d39cd801c97da03008015be0ca7c37f0375
Emile Shodiev <emile.shodiev@yandex.ru>
2019-07-04 15:06:08 +0300

    telegram commands parser added

---
+++ src/util/parse-command.js
@@ -0,0 +1,12 @@
+const re = /^\/([^\s]+)[\s]+(.+)/gmi;
+
+module.exports = text => {
+    let result = text.match(re);
+
+    if (!result) return null;
+
+    return {
+        command: result[1].toLowerCase(),
+        arguments: result[2].split(/[\s]+/g).map(a => a.toLowerCase())
+    }
+}
