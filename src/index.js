d1f6077e48be26befc7d7c70d4674aaca2e16fb0
Emile Shodiev <emile.shodiev@yandex.ru>
2019-07-05 18:20:05 +0300

    index cleanup

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ let config = require("./config")
 let getDbAdapter = require("./db/sqlite-adapter");
 let { EventEmitter } = require("events");
 let Crawler = require("./crawler")
-let { createDefaultParser } = require("./parser/parse-manager")
+let { createDefaultParser, getDefaultParser } = require("./parser/parse-manager")
 let startBot = require("./bot")
+let loadAndParse = require("./util/load-and-parse")
 
 let mediator = new EventEmitter();
 
@@ -15,60 +16,9 @@ puppeteer.launch().then(browser => {
     createDefaultParser(browser);
     return getDbAdapter(mediator);
 }).then(async db => {
-    let crawler = new Crawler(mediator, db);
-    crawler.start();
-
-    startBot(mediator, db)
-    // db.addItem("https://street-beat.ru/d/krossovki-nike-ao4971-002/")
-    //     .then(() => db.getItems())
-    //     .then(console.log)
-    
-    // db.getItems().then(console.log)
-
-    // db.addWishByUrl("some_new_use2r2",
-    //     "https://street-beat.ru/d/krossovki-nike-ao4971-002/")
-    //     .then(() => db.getWishes())
-    //     .then(console.log)
-    //     .catch(console.error)
-    
+    let crawler = new Crawler(mediator, db).start();
+    startBot(mediator, db);
 }).catch(err => {
     throw new Error(err)
 });
 
-
-
-
-(async () => {
-    // let response = await axios.get("https://street-beat.ru/d/krossovki-new-balance-ml574obc-d/", {
-    //     headers: {
-    //         "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
-    //     }
-    // });
-    // let $ = cheerio.load(response.data);
-    // console.log(response.data)
-    // let article = $(".product-article")
-    // console.log(article)
-
-    console.time("launching browser")
-    let browser = await pup.launch();
-    console.timeEnd("launching browser")
-    
-    console.time("new page")
-    let page = await browser.newPage();    
-    console.timeEnd("new page")
-    
-    console.time("goto")
-    await page.goto("https://street-beat.ru/d/krossovki-new-balance-ml574obc-d/")
-    console.timeEnd("goto")
-
-    console.time("cont")
-    let html = await page.content()
-    console.timeEnd("cont")
-
-    let $ = cheerio.load(html)
-
-    console.log($(".product-article").text())
-
-    // console.log(html)
-    await browser.close();
-});
