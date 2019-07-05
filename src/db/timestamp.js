5a032e12c98f48d4ea49b47b71989ae3677fe8db
Emile Shodiev <emile.shodiev@yandex.ru>
2019-07-05 18:16:20 +0300

    timestamp module added

---
+++ src/db/timestamp.js
@@ -0,0 +1,8 @@
+
+const moment = require("moment");
+const timestampFormat = "YYYY-MM-DD HH:mm:ss";
+
+module.exports = {
+    getTimestamp: () => moment().format(timestampFormat),
+    fromTimestamp: ts => moment(ts, timestampFormat)
+}
